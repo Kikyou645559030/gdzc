9c9b5281345e768954f2d4de0cc58f4666faa0da
liuzhichao <liu645559030@qq.com>
2014-06-10 23:57:34 +0800

    修改排序算法

--- src/main/java/com/gdzc/study/Algorithm.java
+++ src/main/java/com/gdzc/study/Algorithm.java
@@ -4,21 +4,41 @@ package com.gdzc.study;
  * Created by Liu_Zhichao on 14-6-9.
  */
 public class Algorithm {
+    //args是javac时候后面跟着的参数
     public static void main(String[] args){
+        Integer[] arrayList = new Integer[args.length];
         if (args.length > 0){
-            if (args.length == 1){
-                for (int i = 0; i < args.length - 1; i++){
-                    Integer a = Integer.parseInt(args[i]);
-                    Integer b = Integer.parseInt(args[i+1]);
-                    Integer c = 0;
-                    if (a > b){
-                        c = b;
-                        b = a;
-                        a = c;
-                        args[i] = a.toString();
-                        args[i+1] = b.toString();
+            if (args.length > 1){
+                for (int i = 0; i < args.length;i++){
+                    arrayList[i] = Integer.parseInt(args[i]);
+                }
+                //选择排序，比较出小的立即换位，再比较后面
+                Integer temp;
+                for (int i = 0; i < arrayList.length; i++){
+                    for (int j = i + 1; j < arrayList.length; j++){
+                        if (arrayList[i] > arrayList[j]){
+                            temp = arrayList[j];
+                            arrayList[j] = arrayList[i];
+                            arrayList[i] = temp;
+                        }
                     }
-                    a = a < b ? a : b;
+                }
+
+                //选择排序优化版，比较出最小的再换位
+                int k;
+                Integer t;
+                for (int i = 0;i < arrayList.length; i++){
+                    k = i;
+                    for (int j = k + 1; j < arrayList.length; j++){
+                        if (arrayList[k] > arrayList[j]){
+                            k = j;
+                        }
+                    }
+                    if (k != i){
+                        t = arrayList[i];
+                        arrayList[i] = arrayList[k];
+                        arrayList[k] = t;
+                    }
                 }
             }else {
                 System.out.println(args[0]);
