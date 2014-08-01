c28457a690a4ca2f174b5929690febc35da0cde9
liuzhichao <liu645559030@qq.com>
2014-08-01 22:10:46 +0800

    添加线程创建和启动的测试类

--- src/main/java/com/gdzc/study/TestThread1.java
+++ src/main/java/com/gdzc/study/TestThread1.java
@@ -2,6 +2,7 @@ package com.gdzc.study;
 
 /**
  * Created by Liu_Zhichao on 14-8-1.
+ * 实现Runnable接口,new Thread类来启动线程
  */
 public class TestThread1 {
     public static void main(String[] args){
---
+++ src/main/java/com/gdzc/study/TestThread2.java
@@ -0,0 +1,23 @@
+package com.gdzc.study;
+
+/**
+ * Created by Liu_Zhichao on 14-8-1.
+ * 继承Thread类,可以直接启动线程
+ */
+public class TestThread2 {
+    public static void main(String[] args){
+        Runner1 r = new Runner1();
+        r.start();
+        for (int i = 0;i < 100;i++){
+            System.out.println("Main Thread"+i);
+        }
+    }
+}
+
+class Runner1 extends Thread{
+    public void run(){
+        for (int i = 0;i < 100;i++){
+            System.out.println("Runner Thread"+i);
+        }
+    }
+}
