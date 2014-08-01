78a7da2b8e3eef232351d9d7c8d4eea4f93a21ec
liuzhichao <liu645559030@qq.com>
2014-08-01 18:06:01 +0800

    添加线程创建和启动的测试类

---
+++ src/main/java/com/gdzc/study/TestThread1.java
@@ -0,0 +1,26 @@
+package com.gdzc.study;
+
+/**
+ * Created by Liu_Zhichao on 14-8-1.
+ */
+public class TestThread1 {
+    public static void main(String[] args){
+        Runner r = new Runner();
+//        r.run();//方法的调用,不是启动线程
+        Thread t = new Thread(r);//new一个线程,参数是实现了Runnable接口的对象
+        t.start();//启动线程
+        for (int i = 0;i < 100;i++){
+            System.out.println("Main Thread:"+i);
+        }
+    }
+}
+
+class Runner implements Runnable{
+
+    @Override
+    public void run() {
+        for (int i = 0;i < 100;i++){
+            System.out.println("Runner Thread:"+i);
+        }
+    }
+}
