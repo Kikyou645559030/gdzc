b351c01c7d8a2cb69b2a185b7aaf43934ada9053
liuzhichao <liu645559030@qq.com>
2014-08-25 23:47:46 +0800

    在反射的测试类中添加测试代码

--- src/main/java/com/gdzc/study/heima/note/Reflect.java
+++ src/main/java/com/gdzc/study/heima/note/Reflect.java
@@ -1,24 +1,73 @@
 package com.gdzc.study.heima.note;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 /**
  * Created by Liu_Zhichao on 14-8-25.
  * 反射
  */
 public class Reflect {
     public static void main(String[] args){
-        Class c = (new Reflect()).getClass();
-        System.out.println(c.toString());
-        System.out.println(c.asSubclass(c));
-        System.out.println(c.getCanonicalName());
-        System.out.println(c.getClassLoader());
-        System.out.println(c.getComponentType());
-        System.out.println(c.getConstructors());
-        System.out.println(c.getDeclaredFields());
-        System.out.println(c.getDeclaredMethods());
-        System.out.println(c.getModifiers());
-        System.out.println(c.getPackage());
-        System.out.println(c.getSimpleName());
-        System.out.println(c.getSuperclass());
-        System.out.println(c.getTypeParameters());
+        try {
+            //作用:返回这个类的字节码的三种方式c1/c2/c3，下面这种方式有两种区别
+            // 1.如果这个类已经加载到内存就直接获得
+            // 2.如果这个类还没有加载到内存，就要先调用类加载器加载到内存中，保存字节码
+            Class c1 = Class.forName("java.lang.String");
+
+            Class c2 = Reflect.class;
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        Class c3 = (new Reflect()).getClass();
+        System.out.println(c3.isPrimitive());//false判断字节码是不是基础数据类型的
+        System.out.println(int.class.isPrimitive());//true
+        System.out.println(int.class == Integer.class);//false
+        System.out.println(int.class == Integer.TYPE);//true代表Integer所包装的基础数据类型的字节码
+        System.out.println(int[].class.isPrimitive());//false
+        System.out.println(int[].class.isArray());//true
+
+        System.out.println(c3.toString());
+        System.out.println(c3.asSubclass(c3));
+        System.out.println(c3.getCanonicalName());
+        System.out.println(c3.getClassLoader());
+        System.out.println(c3.getComponentType());
+        System.out.println(c3.getConstructors());
+        System.out.println(c3.getDeclaredFields());
+        System.out.println(c3.getDeclaredMethods());
+        System.out.println(c3.getModifiers());
+        System.out.println(c3.getPackage());
+        System.out.println(c3.getSimpleName());
+        System.out.println(c3.getSuperclass());
+        System.out.println(c3.getTypeParameters());
+
+        //new String(new StringBuffer("abc"));通过构造方法来实现
+        //获取只有一个参数，参数类型为StringBuffer的构造方法对象
+        try {
+            Constructor constructor = String.class.getConstructor(StringBuffer.class);//得到某一个构造方法
+            String s = (String)constructor.newInstance(new StringBuffer("abc"));//创建实例对象
+            System.out.println(s.charAt(2));
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+
+        //该方法内部先得到默认的构造方法，再用这个构造方法来创建实例对象
+        try {
+            String obj = (String)Class.forName("java.lang.String").newInstance();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        //九种预定义的Class实例对象,八种基础类型加void
+        Class clazz = void.class;
     }
 }
