69d3bb8d9b8597447ea70db2496c4bf7e4166d22
liuzhichao <liu645559030@qq.com>
2014-08-27 00:35:07 +0800

    在反射的测试类中添加反射开发框架的原理的测试代码

--- src/main/java/com/gdzc/study/heima/note/Reflect.java
+++ src/main/java/com/gdzc/study/heima/note/Reflect.java
@@ -1,14 +1,15 @@
 package com.gdzc.study.heima.note;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.util.Arrays;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.*;
+import java.util.*;
 
 /**
  * Created by Liu_Zhichao on 14-8-25.
- * 反射
+ * 反射--作用:实现框架功能
  */
 public class Reflect {
     public static void main(String[] args){
@@ -26,10 +27,10 @@ public class Reflect {
             e.printStackTrace();
         }
         Class c3 = (new Reflect()).getClass();
-        System.out.println("Class:" + c3.isPrimitive());//false判断字节码是不是基础数据类型的
+        System.out.println("Class:" + c3.isPrimitive());//false 判断字节码是不是基础数据类型的
         System.out.println("Class:" + int.class.isPrimitive());//true
         System.out.println("Class:" + (int.class == Integer.class));//false
-        System.out.println("Class:" + (int.class == Integer.TYPE));//true代表Integer所包装的基础数据类型的字节码
+        System.out.println("Class:" + (int.class == Integer.TYPE));//true 代表Integer所包装的基础数据类型的字节码
         System.out.println("Class:" + int[].class.isPrimitive());//false
         System.out.println("Class:" + int[].class.isArray());//true
 
@@ -154,15 +155,85 @@ public class Reflect {
         System.out.println("Array:" + (arr1.getClass() == arr2.getClass()));
 //        System.out.println(arr1.getClass() == arr3.getClass());
 //        System.out.println(arr1.getClass() == arr4.getClass());
-        System.out.println(arr1.getClass().getName());
-        System.out.println(arr1.getClass().getSuperclass().getName());
-        System.out.println(arr4.getClass().getSuperclass().getName());
+        System.out.println("Array:" + arr1.getClass().getName());
+        System.out.println("Array:" + arr1.getClass().getSuperclass().getName());
+        System.out.println("Array:" + arr4.getClass().getSuperclass().getName());
         Object obj1 = arr1;//由于int是基本数据类型，所以不能用Object[]来表示，arr1只能是一个对象
         Object[] obj2 = arr3;//obj2的每一个元素都是一个一维数组
         Object[] obj3 = arr4;//obj3的每一个元素就是一个String对象，非基本数据类型既可以当做Object来用也可以当做Object[]来用
-        System.out.println(arr1);//打印的是一个对象地址
+        System.out.println("Array:" + arr1);//打印的是一个对象地址
         //1.4中asList()接受的参数是Object[]，1.5接受的是可变参数
-        System.out.println(Arrays.asList(arr1));//会当做1.5的语法处理，所以当做一个数组对象
-        System.out.println(Arrays.asList(arr4));//会当做1.4的语法处理
+        System.out.println("Array:" + Arrays.asList(arr1));//会当做1.5的语法处理，所以当做一个数组对象
+        System.out.println("Array:" + Arrays.asList(arr4));//会当做1.4的语法处理
+        printObject(arr1);
+
+        /*
+        HashCode的作用
+        */
+        //当集合是Hash类型的时候，才会用到HashCode，作用是提高效率
+        Collection<ReflectPoint> list = new ArrayList<ReflectPoint>();
+        Collection<ReflectPoint> set = new HashSet<ReflectPoint>();
+        ReflectPoint rp2 = new ReflectPoint(3,3);
+        ReflectPoint rp3 = new ReflectPoint(5,5);
+        ReflectPoint rp4 = new ReflectPoint(3,3);
+        list.add(rp2);
+        list.add(rp3);
+        list.add(rp4);
+        //Hash集合，先计算HashCode值，相同就不放到set中
+        set.add(rp2);
+        set.add(rp3);
+        set.add(rp4);
+        //当add完成之后，再来修改类中参与HashCode计算的变量
+        // 那么对象的HashCode值也会改变，由于equals()方法是先用==比较的
+        // 所以原先相等的两个对象现在不相等了，下面调用remove()方法无效了
+        // 有可能会出现内存泄漏(有一块内存没有使用，但是也没有被释放就会出现)
+        rp2.y = 7;
+        set.remove(rp2);//删不了的原因是HashCode值变了
+        System.out.println(list.size());
+        System.out.println(set.size());
+        rp2.y = 3;
+        set.remove(rp2);
+        System.out.println(set.size());
+
+        /*
+        框架的概念，用反射开发框架的原理
+        */
+        try {
+            //通过类加载器来读取配置文件,配置文件必须要放在classpath下
+            InputStream is = Reflect.class.getClassLoader().getResourceAsStream("config.properties");
+            //通过流来读取配置文件
+            InputStream fis = new FileInputStream("src/main/resources/config.properties");
+            Properties properties = new Properties();
+            properties.load(fis);
+            fis.close();
+            //获取文件中的配置信息
+            String className = properties.getProperty("className");
+            //根据配置信息创建实例对象
+            Collection cc = (Collection)Class.forName(className).newInstance();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //通过数组的反射来获取数组的长度或者某一个元素
+    private static void printObject(Object obj) {
+        Class c = obj.getClass();
+        if (c.isArray()){//判断obj是否为数组
+            int len = Array.getLength(obj);//通过Array来获取数组长度
+            for (int i = 0; i < len; i++){
+                System.out.println("Array:" + Array.get(obj, i));//通过Array来拿到数组的某一个元素
+                System.out.println("Array:" + Array.get(obj,i).getClass().getName());//拿到某个元素的类型
+            }
+        }else {
+            System.out.println("Array:" + obj);
+        }
     }
 }
--- src/main/java/com/gdzc/study/heima/note/ReflectPoint.java
+++ src/main/java/com/gdzc/study/heima/note/ReflectPoint.java
@@ -22,4 +22,24 @@ public class ReflectPoint {
             System.out.println(s);
         }
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        ReflectPoint that = (ReflectPoint) o;
+
+        if (x != that.x) return false;
+        if (y != that.y) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = x;
+        result = 31 * result + y;
+        return result;
+    }
 }
