39ddc9845b4f9cdee843825959032237fe4a665d
Kikyou645559030 <liu199211032813>
2015-06-02 14:35:35 +0800

    修改测试类

---
+++ src/test/java/Test.java
@@ -0,0 +1,10 @@
+/**
+ * 测试
+ * Created by Liu_Zhichao on 2015/3/3 0003.
+ */
+public class Test {
+	public static void main(String[] args) {
+		String s = "abcdefghijkl";
+		System.out.println(s.length() + "," + s.substring(s.length() - 11));
+	}
+}
